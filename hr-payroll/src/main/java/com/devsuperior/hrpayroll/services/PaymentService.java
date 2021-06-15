90feeaae398f3a1716a60c84c2075526a2299145
Anderson Silva <anderson.silva@topi.io>
2021-06-15 14:58:25 -0300

    Add Feign client

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,14 +1,24 @@
 package com.devsuperior.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hrpayroll.entities.Payment;
+import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 
-	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.00, days);
-	}
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
 	
+	public Payment getPayment(Long workerId, int days) {
+
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+
+	}
+
 }
