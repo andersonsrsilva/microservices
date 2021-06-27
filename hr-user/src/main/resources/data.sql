8d5b8ccfb715191cd9886753fd84ba7ef59237fd
Anderson Silva <anderson.silva@topi.io>
2021-06-27 12:24:54 -0300

    UserRepository, UserResource e Zuul config

--- hr-user/src/main/resources/data.sql
+++ hr-user/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Anderson Silva', 'adersonsrsilva@gmail.com', '$2a$10$YfO30G5PtpnlQhVx8nxkR.FEIr1d/s6535qlkfqlxGvzsWGcCdz1m');
+INSERT INTO tb_user (name, email, password) VALUES ('Anderson Silva', 'anderson@gmail.com', '$2a$10$YfO30G5PtpnlQhVx8nxkR.FEIr1d/s6535qlkfqlxGvzsWGcCdz1m');
 INSERT INTO tb_user (name, email, password) VALUES ('Joice Silva', 'joice@gmail.com', '$2a$10$YfO30G5PtpnlQhVx8nxkR.FEIr1d/s6535qlkfqlxGvzsWGcCdz1m');
 
 INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
